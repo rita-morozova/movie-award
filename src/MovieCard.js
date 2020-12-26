9c9720090910016e898871f5d7affc381338b7ef
Margarita Morozova <ritamorozova@yahoo.com>
2020-12-26 11:26:13 -0800

    handling broken url

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const MovieCard = ({movie, addToNomination,  index, disabledButtons}) =>{
 
+  const addDefaultSrc = (ev) =>{
+    ev.target.src ='/poster.jpg'
+  }
 
     return(
       <div >
+        <img onError={addDefaultSrc} src={movie.Poster} alt='poster' />
         <h2>{movie.Title}</h2>
         <h3>{movie.Year}</h3>
         <button onClick={() => addToNomination(index, movie)} disabled={disabledButtons[index]}>Nominate</button>
