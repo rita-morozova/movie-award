2c80c1d426e2808e14ba02f67172ae0a01859f63
Margarita Morozova <ritamorozova@yahoo.com>
2020-12-28 16:57:55 -0800

    final styling for header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Icon} from 'semantic-ui-react'
 
 const Header = () => {
 
   return(
     <div className='header'>
-      <h1>The Shoppies: Movie awards for entrepreneurs</h1>
+      <Icon name='winner' size='large' className='winner' />
+      <p>The Shoppies: Movie awards for entrepreneurs</p>
     </div>
   )
 }
