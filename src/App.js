f9fa2ae43d7828667d473adeecd2c731701c9da3
Margarita Morozova <ritamorozova@yahoo.com>
2020-12-29 08:51:57 -0800

    handling null

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ state = {
 
 componentDidMount = () => {
   let nominations = localStorage.getItem('nominations')
-  if(nominations.length !== 0){
+  if(nominations.length !== 0 || nominations !== null){
     this.setState({nominations: JSON.parse(nominations)})
   }
 }
